2d0585d3cf9d759696b46c269cc720858c2aabc6
Robin9298 <94828062+Robin9298@users.noreply.github.com>
2022-08-03 22:05:35 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+student1 = "hema"
+print (student1)
+
+
+# In[5]:
+
+
+student2  = """swaroop"""
+print (student2)
+
+
+# In[6]:
+
+
+student3 = 'nusrat'
+print (student3)
+
+
+
+
+
